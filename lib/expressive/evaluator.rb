25efbe639040a7e8839146e7baacb5c900d49b1a
Omkar Moghe <theomkarmoghe@gmail.com>
2024-03-24 14:22:35 -0700

    eval coment

--- lib/expressive/evaluator.rb
+++ lib/expressive/evaluator.rb
@@ -1,6 +1,8 @@
 require 'delegate'
 
 module Expressive
+  # Used to wrap `operands` when evaluating an `Expression`. This allows us to "extend" the functionality of an object
+  # without polluting the app wide definition.
   class Evaluator < SimpleDelegator
     def and(other)
       __getobj__ && other.__getobj__
